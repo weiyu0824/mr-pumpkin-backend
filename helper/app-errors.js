16b5730e13dff7c1eedd5a8e125a5e04496ba6e2
weiyu <jcab1688@gmail.com>
2022-01-08 15:53:20 +0800

    Define several errors

--- helper/app-errors.js
+++ helper/app-errors.js
@@ -8,18 +8,29 @@ class BaseError extends Error {
     }
 };
 class ApiError extends BaseError {
-    constructor(message="This api does no exist", statusCode) {
+    constructor(message="This api does no exist.", statusCode) {
         super(message, statusCode);
     }
 }
 class UnAuthorizedError extends BaseError {
-    constructor(message="The resources you are aquiring is unauthorized", statusCode){
+    constructor(message="The resources you are aquiring is unauthorized.", statusCode){
+        super(message, statusCode);
+    }
+}
+class UsernameError extends BaseError {
+    constructor (message="The username you entered doesn't belong to an account. Please check your username and try again.", statusCode) {
+        super(message, statusCode);
+    }
+}
+class PasswordError extends BaseError {
+    constructor(message="Sorry, your password was incorrect. Please double-check your password.", statusCode){
         super(message, statusCode);
     }
 }
 
 export default {
     ApiError,
-    DataNotFoundError,
+    UnAuthorizedError,
+    UsernameError,
     PasswordError
 }
